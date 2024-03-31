4ddb2a044f39bc2bb8c9ff4b0ceff8f4a37e9a68
Vita <vitatriutami@gmail.com>
2024-03-31 17:42:10 +0700

    add starter frontend

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt='' className='logo'/>
+      <ul className='navbar-menu'>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
