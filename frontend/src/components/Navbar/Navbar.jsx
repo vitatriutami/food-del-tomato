2750faa27639133862f352e9930cc9e4effaaf3b
Vita <vitatriutami@gmail.com>
2024-04-03 14:37:01 +0700

    update navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,8 +7,19 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={assets.logo} alt='' className='logo'/>
       <ul className='navbar-menu'>
-        
+        <li>Home</li>
+        <li>Home</li>
+        <li>Mobile App</li>
+        <li>Contact us</li>
       </ul>
+      <div className='navbar-right'>
+        <img src={assets.search_icon} alt='' />
+        <div className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt='' />
+          <div className='dot'></div>
+        </div>
+        <button>Sign In</button>
+      </div>
     </div>
   )
 }
