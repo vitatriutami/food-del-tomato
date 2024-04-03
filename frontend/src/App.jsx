7b9a77af054e7ec67ff09925b731c8371caf84a9
Vita <vitatriutami@gmail.com>
2024-04-03 16:11:06 +0700

    add route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes/>
     </div>
   )
 }
