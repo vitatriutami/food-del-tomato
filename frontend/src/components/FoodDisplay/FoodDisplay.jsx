f3e786d251369908b7c4c7fd96637e2733e8da42
Vita <vitatriutami@gmail.com>
2024-04-06 04:19:33 +0700

    add food-item

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
   
@@ -9,6 +10,11 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
+      <div className='food-display-list'>
+        {food_list.map((item,index)=>{
+          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+        })}
+      </div>
     </div>
   )
 }
