6be6a6e5e88f69e96b377c6bc7d2f513ddfa8c9d
Vita <vitatriutami@gmail.com>
2024-04-14 07:05:06 +0700

    add login-popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} />:<></>}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
