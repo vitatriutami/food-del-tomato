0b4b6190ffac394f40ea45880ef2c5b806d2f26e
Vita <vitatriutami@gmail.com>
2024-04-17 02:45:41 +0700

    update place-order

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext';
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
     <form className="place-order">
       <div className="place-order-left">
@@ -20,7 +24,30 @@ const PlaceOrder = () => {
           <input type="text" placeholder="Zip code" />
           <input type="text" placeholder="Country" />
         </div>
-        <input type='text' placeholder='Phone' />
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className='place-order-right'>
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <div className="cart-toal-details">
+              <b>Delivery Fee</b>
+              <b>${2}</b>
+            </div>
+            <hr />
+            <div className="cart-toal-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>
+            PROCEED TO PAYMENT
+          </button>
+        </div>
       </div>
     </form>
   );
