04f1d0d506f42086d56afc77db8825c02c43b205
Vita <vitatriutami@gmail.com>
2024-04-17 02:38:36 +0700

    add place-order

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import './Cart.css'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
   
   const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext);
-  
+  const navigate = useNavigate()
+
   return (
     <div className="cart">
       <div className="cart-items">
@@ -56,7 +58,7 @@ const Cart = () => {
               <b>${getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,10 +3,27 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Last name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type='text' placeholder='Phone' />
+      </div>
+    </form>
+  );
 }
 
 export default PlaceOrder
